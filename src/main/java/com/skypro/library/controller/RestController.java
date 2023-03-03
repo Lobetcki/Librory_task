a74b3e961436bf5f99bacdd15c4739664c4c6fb0
Anton Mikhailouski <Lobetcki@gmail.com>
2023-03-03 16:35:55 +0300

    Library /Library_03032023_1

--- src/main/java/com/skypro/library/controller/RestController.java
+++ src/main/java/com/skypro/library/controller/RestController.java
@@ -4,45 +4,44 @@ import com.skypro.library.entity.Books;
 import com.skypro.library.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 import java.util.List;
 
-@RestController
+@org.springframework.web.bind.annotation.RestController
 @RequestMapping("/library")
-public class BookController {
+class RestController {
 
     private BookService bookService;
 
-    public BookController() {
+    public RestController() {
     }
 
-    public BookController(BookService bookService) {
+    public RestController(BookService bookService) {
         this.bookService = bookService;
     }
 
-    @GetMapping("/book")                    //4. Получение всех книг
+    @GetMapping("/web")                    //4. Получение всех книг
     public List<Books> getBooksController() {
         return bookService.getBooksService();
     }
 
-    @GetMapping("/book/{isbn}")                //5. Получение одной книги по ISBN
+    @GetMapping("/api/book/{isbn}")                //5. Получение одной книги по ISBN
     public Books getBookByIsbnController(@PathVariable Character isbn){
         return bookService.getBookByIsbnService(isbn);
     }
 
-    @PostMapping("/book")                   //1. Создание книги с помощью запроса INSERT
+    @PostMapping("/api/book")                   //1. Создание книги с помощью запроса INSERT
     public Books addBookController(@RequestBody Books book) {
         bookService.addEBookService(book);
         return book;
     }
 
-    @PutMapping("/book")                    //2. Изменение названия, автора или года издания книги с помощью одного запроса UPDATE который принимает все три параметра
+    @PutMapping("/api/book")                    //2. Изменение названия, автора или года издания книги с помощью одного запроса UPDATE который принимает все три параметра
     public Books updateBookController(@RequestBody Books book) {
         bookService.updateBookService(book);
         return book;
     }
 
-    @DeleteMapping("/book/{isbn}")                  //3. Удаление книги по ISBN
+    @DeleteMapping("/api/book?isbn=<isbn>")                  //3. Удаление книги по ISBN
     public String deleteBookController(@PathVariable Character isbn) {
         bookService.deleteBookService(isbn);
         return "Person with id = " + isbn + " was deleted";
